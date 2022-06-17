2b8d32614f5ef335dd939137e66ba0682c78ca9b
alexiusbond <aleksey.bondarev@iaau.edu.kg>
2022-06-17 21:44:30 +0600

    Added new comment

--- lesson_5_1.py
+++ lesson_5_1.py
@@ -1,4 +1,4 @@
-#from calculator import addition, multiplication
+# from calculator import addition, multiplication
 from calculator import addition as add
 from person import Person
 from random import randint as genarate_random_number
@@ -7,9 +7,9 @@ import os
 from envparse import env
 
 print(add(88, 1))
-
+# Hi teacher
 person = Person("John", 22)
-termcolor.cprint('Hello, World!',  'green', attrs=['underline'])
+termcolor.cprint('Hello, World!', 'green', attrs=['underline'])
 
 print(os.environ.get('LOGIN_FOR_DB'))
 
